32482e2871339a662c654df7a0b8a0b29af06a55
Andrew <andrewallison5211@gmail.com>
2023-07-20 16:51:34 -0600

    Blogger

---
+++ src/pages/ProfilesPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-10 m-auto mt-5 ">
+        <div class="d-flex p-4">
+          <img :src="AVATAR" alt="Picture">
+          <h2 class="p-4 ms-5">AUTHOR</h2>
+          <button>Edit</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  </template>
+
+
+<script>
+export default {
+  setup(){
+    return {
+      
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
